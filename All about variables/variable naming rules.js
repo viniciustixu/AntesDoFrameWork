0ef41555a42b34116beed2e0af1c5872b6e0c528
Vinicius Souza <vinicipkt@live.com>
2023-06-10 10:45:56 -0300

    variable naming rules

---
+++ All about variables/variable naming rules.js
@@ -0,0 +1,31 @@
+/* As regras de nomenclatura de variáveis em JavaScript são as seguintes:
+
+1. Os nomes de variáveis devem começar com uma letra (a-z, A-Z) ou com o caractere de sublinhado (_), ou com o símbolo do dólar ($). Eles não podem começar com um número.
+
+2. Os nomes de variáveis podem conter letras, números, sublinhados (_) ou símbolos do dólar ($). No entanto, não é recomendado usar o símbolo do dólar no início de um nome
+de variável, pois geralmente é reservado para uso interno de bibliotecas JavaScript.
+
+3. Os nomes de variáveis são case-sensitive, o que significa que "myVariable" e "myvariable" são considerados nomes de variáveis diferentes.
+
+4. Não é possível usar palavras reservadas do JavaScript como nomes de variáveis. Por exemplo, as palavras "var", "function" e "if" são palavras reservadas e não podem ser usadas
+como nomes de variáveis.
+
+5. É recomendado usar nomes descritivos e significativos para suas variáveis, para que seu código seja mais legível e compreensível. Por exemplo, em vez de usar um nome como "x",
+é melhor usar algo como "count" ou "total". */
+
+// Aqui estão alguns exemplos de nomes de variáveis válidos em JavaScript: 
+
+
+var myVariable;
+var _privateVariable;
+var $specialVariable;
+var firstName;
+var age1;
+
+// Lembre-se de que seguir essas regras de nomenclatura ajudará a manter seu código mais consistente e facilitará a leitura e a manutenção.
+
+/* Camel Casing: a primeira letra de cada palavra é maiúscula, exceto a primeira palavra. Camel case é a convenção de escrita utilizada em JavaScript.
+Aqui estão alguns exemplos de nomes de identificadores em JavaScript:
+indexOf,
+getElementById,
+querySelectorAll. */
