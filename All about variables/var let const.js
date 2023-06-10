fd3b1b5fcb304d1601751f03d440137741d8a9ff
Vinicius Souza <vinicipkt@live.com>
2023-06-10 10:31:11 -0300

    var let const

---
+++ All about variables/var let const.js
@@ -0,0 +1,5 @@
+var x = "variável 1" // var's tem escopo global. Var's são içadas (hoisted) o que significa que elas podem ser usadas antes de serem declaradas no código.
+
+let y = "variável 2" // let's tem escopo de bloco e podem ser reatribuidas com um novo valor assim como as var's.
+
+const z = "variável 3" /* const's tem escopo de bloco e não podem ser reatribuidas, no entanto, isso não significa que as variáveis const são imutáveis, pois se forem declaradas como objetos ou arrays, suas propriedades ou elementos podem ser alterados. */
