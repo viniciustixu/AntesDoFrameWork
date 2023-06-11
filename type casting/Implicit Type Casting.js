14a7ac49a95b86624affb3c9d9ce13f336771a5e
Vinicius Souza <vinicipkt@live.com>
2023-06-11 10:06:06 -0300

    implicit type casting

---
+++ type casting/Implicit Type Casting.js
@@ -0,0 +1,4 @@
+/* Implicit Type Casting
+
+A conversão implícita de tipos ocorre quando o compilador ou tempo de execução converte automaticamente os tipos de dados. JavaScript é uma linguagem de tipagem fraca e na maioria
+das vezes os operadores convertem automaticamente um valor para o tipo correto. */
