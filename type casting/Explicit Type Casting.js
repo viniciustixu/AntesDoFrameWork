e1c66dd7e98d0411aa6d328d69070f214cd2ada1
Vinicius Souza <vinicipkt@live.com>
2023-06-11 10:05:09 -0300

    Explicit Type Casting

---
+++ type casting/Explicit Type Casting.js
@@ -0,0 +1,4 @@
+/* Explicit Type Casting
+
+Casting de tipos significa transferir dados de um tipo de dados para outro, especificando explicitamente o tipo para converter os dados fornecidos. O casting explícito
+de tipos é normalmente feito para tornar os dados compatíveis com outras variáveis. Exemplos de métodos de casting são parseInt(), parseFloat() e toString(). */
