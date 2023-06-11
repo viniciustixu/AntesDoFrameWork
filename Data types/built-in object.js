b46174e113e7295f68e11ef0d4e8c4d76872f393
Vinicius Souza <vinicipkt@live.com>
2023-06-11 09:35:57 -0300

    built-in object

---
+++ Data types/built-in object.js
@@ -0,0 +1,27 @@
+/* Os "built-in objects" (objetos embutidos) em JavaScript são objetos pré-definidos que estão disponíveis globalmente no ambiente de execução do JavaScript. Esses objetos fornecem
+funcionalidades comuns e tipos de dados úteis para várias tarefas de programação.
+
+Aqui estão alguns exemplos de objetos embutidos em JavaScript:
+
+1.Object: É o objeto base para todos os objetos JavaScript. Ele fornece métodos e propriedades para criar e manipular objetos.
+
+2.Array: É usado para criar e manipular arrays, que são coleções ordenadas de valores.
+
+3.String: É usado para criar e manipular strings, que são sequências de caracteres.
+
+4.Number: É usado para criar e manipular valores numéricos.
+
+5.Boolean: É usado para criar e manipular valores booleanos (verdadeiro ou falso).
+
+6.Function: É usado para criar funções, que são blocos de código reutilizáveis.
+
+7.Date: É usado para trabalhar com datas e horários.
+
+8.Math: É usado para realizar operações matemáticas avançadas, como cálculos de raiz quadrada, logaritmos, etc.
+
+9.RegExp: É usado para criar e manipular expressões regulares, que são padrões de texto usados para realizar correspondências e extrações complexas em strings.
+
+10.Error: É usado para criar objetos de erro, que representam erros ocorridos durante a execução do programa.
+
+Esses são apenas alguns dos objetos embutidos em JavaScript. Existem outros objetos embutidos, como Map, Set, Promise, entre outros, que fornecem funcionalidades adicionais para
+tarefas específicas. Além disso, é possível criar objetos personalizados usando a função construtora Object ou a sintaxe de classe em JavaScript. */
