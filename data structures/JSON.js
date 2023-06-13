85d63d20ec53f5f23554ec0c6b6731685ee99746
Vinicius Souza <vinicipkt@live.com>
2023-06-13 13:51:34 -0300

    JSON

---
+++ data structures/JSON.js
@@ -0,0 +1,26 @@
+/* JSON (JavaScript Object Notation) é um formato leve de troca de dados. Ele é amplamente utilizado para transmitir e armazenar dados estruturados de forma legível tanto para humanos
+quanto para máquinas. O JSON é baseado em um subconjunto da linguagem de programação JavaScript, mas pode ser utilizado com várias outras linguagens de programação.
+
+O formato JSON utiliza uma estrutura de pares chave-valor para representar os dados. Os valores podem ser de diferentes tipos, como strings (cadeias de caracteres), números, booleanos,
+arrays (vetores) e objetos. Aqui está um exemplo básico de um objeto JSON: */
+
+    {
+        "nome": "João",
+        "idade": 25,
+        "cidade": "São Paulo"
+    }
+
+/* Nesse exemplo, temos um objeto com três pares chave-valor. A chave "nome" tem o valor "João", a chave "idade" tem o valor 25 e a chave "cidade" tem o valor "São Paulo".
+
+Além de objetos, o JSON também permite o uso de arrays para representar listas de valores. Aqui está um exemplo de um array JSON: */
+
+    {
+        "frutas": ["maçã", "banana", "laranja"]
+    }
+    
+/* Nesse exemplo, temos um objeto com uma chave "frutas" que contém um array com três elementos: "maçã", "banana" e "laranja".
+
+O JSON é amplamente utilizado em aplicações web e serviços de API para transmitir e armazenar dados. Ele é facilmente interpretado por diferentes linguagens de programação
+e pode ser convertido em objetos ou estruturas de dados nativas dessas linguagens.
+
+Espero que isso esclareça o que você precisa saber sobre JSON em português. Se você tiver mais alguma pergunta, sinta-se à vontade para perguntar! */
