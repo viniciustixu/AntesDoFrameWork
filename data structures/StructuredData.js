74cf5ece5dcc01ebea60a88564e11bc4389cd1fc
Vinicius Souza <vinicipkt@live.com>
2023-06-13 13:48:21 -0300

    Structured Data and why to use

---
+++ data structures/StructuredData.js
@@ -0,0 +1,13 @@
+/* Por que adicionar dados estruturados a uma página?
+Adicionar dados estruturados pode ativar resultados de pesquisa mais interessantes para os usuários, chamados de pesquisas aprimoradas, e incentivá-los a interagir mais com seu site.
+Veja alguns estudos de caso de sites que implementaram dados estruturados:
+
+O Rotten Tomatoes adicionou dados estruturados a 100 mil páginas únicas e mediu uma taxa de cliques 25% maior nas páginas aprimoradas com dados estruturados, em comparação às páginas
+sem esse recurso.
+
+O Food Network converteu 80% das páginas deles para ativar os recursos de pesquisa e registrou um aumento de 35% nos acessos.
+
+A Rakuten descobriu que os usuários passam 1,5 vez mais tempo acessando páginas com dados estruturados do que em páginas sem dados estruturados. Além disso, há uma taxa de interação
+3,6 vezes maior em páginas AMP com recursos de pesquisa.
+
+A Nestlé identificou uma taxa de cliques 82% mais alta nas páginas que exibem pesquisas aprimoradas em comparação às que não apresentam o recurso. */
