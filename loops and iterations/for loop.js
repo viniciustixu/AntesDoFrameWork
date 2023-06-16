20d33570ad3a79c60f55c3166c004a62c652d7c5
Vinicius Souza <vinicipkt@live.com>
2023-06-16 18:12:28 -0300

    for loop

---
+++ loops and iterations/for loop.js
@@ -0,0 +1,19 @@
+    for (var i = 0; i < 5; i++) {
+    console.log("O valor de i é: " + i);
+    }
+
+/* Neste exemplo, o loop "for" irá executar o bloco de código repetidamente enquanto a condição i < 5 for verdadeira. A cada iteração, o valor de i é incrementado em 1. No console,
+será exibida a mensagem "O valor de i é:" seguida do valor atual de i.
+
+Você também pode iterar sobre um array usando um loop "for". Aqui está um exemplo: */
+
+    var frutas = ["maçã", "banana", "laranja"];
+
+    for (var i = 0; i < frutas.length; i++) {
+    console.log("A fruta atual é: " + frutas[i]);
+    }
+
+/* Neste caso, o loop "for" irá percorrer o array frutas e exibir cada elemento no console. A variável i representa o índice atual no array, e frutas[i] acessa o elemento
+correspondente.
+
+Espero que isso ajude! Se você tiver mais alguma pergunta, fique à vontade para perguntar. */
