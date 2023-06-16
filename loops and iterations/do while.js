885dd49ef151ffb9003d1876d4685dc20044ab66
Vinicius Souza <vinicipkt@live.com>
2023-06-16 18:15:13 -0300

    do while

---
+++ loops and iterations/do while.js
@@ -0,0 +1,20 @@
+/* O comando "do...while" em JavaScript é usado para criar um loop que executa um bloco de código repetidamente enquanto uma condição específica for verdadeira. A estrutura básica
+do "do...while" é a seguinte: */
+
+do {
+  // Bloco de código a ser executado
+} while (condição);
+
+// Aqui está um exemplo prático do uso do "do...while" em JavaScript:
+
+var contador = 0;
+
+do {
+  console.log("O contador é: " + contador);
+  contador++;
+} while (contador < 5);
+
+/* Neste exemplo, o código irá imprimir o valor do contador e incrementá-lo em 1 repetidamente até que o valor do contador seja menor que 5. Mesmo se a condição não for verdadeira
+desde o início, o bloco de código será executado pelo menos uma vez, porque a verificação da condição é feita após a execução do bloco.
+
+Espero que isso esclareça como usar o comando "do...while" em JavaScript! */
