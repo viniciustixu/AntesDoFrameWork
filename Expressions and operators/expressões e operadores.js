4dd7d1458a1a347b1449ce1b3d3f0c25d30716a4
Vinicius Souza <vinicipkt@live.com>
2023-06-17 17:38:51 -0300

    expressões e operadores

---
+++ Expressions and operators/expressões e operadores.js
@@ -0,0 +1,67 @@
+/* Em JavaScript, existem várias expressões e operadores que podem ser usados para realizar cálculos, comparações e manipulações de dados. Abaixo estão alguns exemplos de expressões
+e operadores comumente usados:
+
+    Operadores Aritméticos:
+    Adição: +
+    Subtração: -
+    Multiplicação: *
+    Divisão: /
+    Módulo (resto da divisão): %
+    Incremento: ++
+    Decremento: --
+Exemplo: */
+
+        let a = 10;
+        let b = 5;
+
+        let soma = a + b; // 15
+        let subtracao = a - b; // 5
+        let multiplicacao = a * b; // 50
+        let divisao = a / b; // 2
+        let resto = a % b; // 0
+
+        a++; // Incremento: a agora é 11
+        b--; // Decremento: b agora é 4
+
+/* Operadores de Atribuição:
+    Atribuição: '='
+    Atribuição com operação: +=, -=, *=, /=, %= (exemplo: a += b é equivalente a a = a + b)
+Exemplo: */
+
+        let c = 10;
+
+        c += 5; // c agora é 15 (equivale c a = c + 5)
+
+/* Operadores de Comparação:
+    Igualdade: ==
+    Desigualdade: !=
+    Igualdade estrita (valor e tipo): ===
+    Desigualdade estrita (valor e tipo): !==
+    Maior que: >
+    Menor que: <
+    Maior ou igual que: >=
+    Menor ou igual que: <=
+Exemplo: */
+
+    let d = 10;
+    let e = 5;
+
+    console.log(d == e); // false
+    console.log(d === e); // false
+    console.log(d > e); // true
+
+/* Operadores Lógicos:
+    E lógico: &&
+    Ou lógico: ||
+    Negação lógica: !
+Exemplo: */
+
+    let f = true;
+    let g = false;
+
+    console.log(f && g); // false
+    console.log(f || g); // true
+    console.log(!f); // false
+
+/* Esses são apenas alguns exemplos de expressões e operadores em JavaScript. A linguagem oferece uma variedade de outros operadores e recursos para manipulação de dados e controle de
+fluxo. */
