da7bce25f41b9d3725c28da3d2fc31604efa9871
Vinicius Souza <vinicipkt@live.com>
2023-06-17 19:02:40 -0300

    built in functions

---
+++ functions/built in functions.js
@@ -0,0 +1,32 @@
+/* As funções internas (built-in functions) em JavaScript são funções pré-definidas fornecidas pela linguagem, que estão disponíveis para uso imediato sem a necessidade de definição
+adicional. Essas funções internas são parte integrante do JavaScript e fornecem funcionalidades comumente usadas.
+
+Aqui estão alguns exemplos de funções internas importantes em JavaScript:
+
+console.log(): Essa função é usada para exibir mensagens no console do navegador ou no console de desenvolvimento do Node.js. É útil para depurar e exibir informações durante a
+execução do programa.
+
+alert(): Essa função exibe um alerta em uma caixa de diálogo no navegador, mostrando uma mensagem ao usuário. É usado principalmente para exibir informações importantes ou solicitar
+ações do usuário.
+
+prompt(): Essa função exibe uma caixa de diálogo no navegador com um campo de entrada de texto, permitindo que o usuário insira um valor. É útil para interagir com o usuário e obter
+dados de entrada.
+
+parseInt(): Essa função converte uma string em um número inteiro. Ela analisa uma string e retorna um número inteiro com base na representação numérica encontrada no início da string.
+
+parseFloat(): Essa função converte uma string em um número de ponto flutuante. Ela analisa uma string e retorna um número de ponto flutuante com base na representação numérica
+encontrada na string.
+
+String(): Essa função converte um valor em uma string. Ela pode ser usada para converter números, booleanos, objetos e outros tipos de dados em strings.
+
+Array.isArray(): Essa função verifica se um determinado valor é um array. Ela retorna true se o valor fornecido for um array e false caso contrário.
+
+Math.random(): Essa função retorna um número aleatório entre 0 (inclusive) e 1 (exclusivo). É comumente usada para gerar números aleatórios em JavaScript.
+
+Math.floor(): Essa função arredonda um número para baixo para o inteiro mais próximo. Ela remove a parte decimal do número e retorna o valor inteiro resultante.
+
+Esses são apenas alguns exemplos das várias funções internas disponíveis em JavaScript. Existem muitas outras funções internas, cada uma com sua própria finalidade e funcionalidade
+específica.
+
+É importante lembrar que as funções internas em JavaScript têm nomes reservados e não devem ser redefinidas. Elas são amplamente usadas para facilitar tarefas comuns e fornecer
+recursos úteis para os desenvolvedores. */
