6156c4bcd692fe485285fbbc635351ebb65aa312
Vinicius Souza <vinicipkt@live.com>
2023-07-02 16:36:09 -0300

    setTimeout

---
+++ Asynchronous JavaScript/settimeout.js
@@ -0,0 +1,18 @@
+/* Em JavaScript, você pode usar a função `setTimeout` para agendar a execução de uma determinada função após um período de tempo especificado. Aqui está um exemplo de como usar
+o `setTimeout` em JavaScript: */
+
+    setTimeout(function() {
+    // Código a ser executado após o tempo especificado
+    }, tempoEmMilissegundos);
+
+/* No exemplo acima, você pode substituir `tempoEmMilissegundos` pelo tempo em milissegundos que deseja esperar antes de executar a função.
+
+Aqui está um exemplo prático que exibe uma mensagem no console após um atraso de 2 segundos: */
+
+    setTimeout(function() {
+    console.log("A mensagem foi exibida após 2 segundos.");
+    }, 2000);
+
+/* O código acima exibirá a mensagem no console após um atraso de 2000 milissegundos, ou seja, 2 segundos.
+
+Espero que isso ajude! Se você tiver mais dúvidas, sinta-se à vontade para perguntar. */
